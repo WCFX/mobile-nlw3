e8ba56683e71e7f29c4f6d65ffd7b344ec90d450
wcfx <wcfx.dev@gmail.com>
2020-10-22 15:51:49 -0300

    Todas as funcoes estaticas criadas.

--- src/screen/SelectMapPosition/index.tsx
+++ src/screen/SelectMapPosition/index.tsx
@@ -1,10 +1,43 @@
 import React from 'react';
-import { View } from 'react-native';
+import { View, Text } from 'react-native';
+import MapView, { Marker } from 'react-native-maps';
+import { RectButton } from 'react-native-gesture-handler';
+import { useNavigation } from '@react-navigation/native';
 
-// import { Container } from './styles';
 
-const SelectMapPosition: React.FC = () => {
-  return <View />;
+import styles from './styles';
+import mapMarkerImg from '../../images/map-marker.png';
+
+const SelectMapPosition = () => {
+
+  const navigation = useNavigation();
+
+  function handleNextStep() {
+    navigation.navigate('OrphanageData');
+  }
+
+  return (
+    <View style={styles.container}>
+      <MapView
+        initialRegion={{
+          latitude: -27.2092052,
+          longitude: -49.6401092,
+          latitudeDelta: 0.008,
+          longitudeDelta: 0.008,
+        }}
+        style={styles.mapStyle}
+      >
+        <Marker
+          icon={mapMarkerImg}
+          coordinate={{ latitude: -27.2092052, longitude: -49.6401092 }}
+        />
+      </MapView>
+
+      <RectButton style={styles.nextButton} onPress={handleNextStep}>
+        <Text style={styles.nextButtonText}>Próximo</Text>
+      </RectButton>
+    </View>
+  );
 }
 
 export default SelectMapPosition;
